da870d8d373e6d654686758b710c95c863c5ff40
YO <ojara@gmx.com>
2024-07-23 10:07:23 +0200

    Initial Commit

---
+++ CALCUL-IOS/TripViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CALCUL-IOS
+//
+//  Created by Mañanas on 22/7/24.
+//
+
+import UIKit
+
+class TripViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
