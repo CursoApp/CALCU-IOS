6ff33d886e364864f3950dbab1c2faacb9a8c32b
YO <ojara@gmx.com>
2024-07-23 14:04:25 +0200

    Mi primer a App IOS

--- CALCUL-IOS/TripViewController.swift
+++ CALCUL-IOS/TripViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+ 
 
 class TripViewController: UIViewController {
+    
+    @IBOutlet weak var heigthLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var height:Int = 160
+    var weight: Float = 70
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onHeightChanged(_ sender: UIStepper) {
+        height = Int(sender.value)
+        heigthLabel.text = "\(sender.value) cm"
+    }
+    
+    @IBAction func onWeightChanged(_ sender: UISlider) {
+        weight = sender.value
+        weightLabel.text = "\(sender.value) kg"
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        print ("Altura:\(height)")
+        print ("Peso:\(weight)")
+    }
+    
 }
 
